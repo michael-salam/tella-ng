10eaa9cb50b867a68c2f047de112a629e2149c03
Donald-Source2023 <donalddyusuf@gmail.com>
2025-06-27 03:36:37 -0700

    Auth

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { app } from "@/firebase";
+import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+// import { signInWithRedirect } from "firebase/auth";
+
+import React from "react";
+
+const Page = () => {
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        const userInfo = {
+          name: user?.displayName,
+          email: user?.email,
+          role: "user",
+        };
+        localStorage.setItem("user", JSON.stringify(userInfo));
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  };
+
+  console.log('current user', auth.currentUser)
+
+  const handleSignOut = () => {
+    signOut(auth)
+    console.log(auth.currentUser)
+  }
+
+  return (
+    <div>
+      <button
+        onClick={handleGoogleSignIn}
+        className="py-5 px-10 cursor-pointer rounded-xl text-white bg-[#10613A]"
+      >
+        Please Sign Up
+      </button>
+
+      <button
+        onClick={handleSignOut}
+        className="py-5 px-10 cursor-pointer rounded-xl text-white bg-[#10613A]"
+      >
+        Sign out
+      </button>
+    </div>
+  );
+};
+
+export default Page;
